5fbf718fc61ee407c2bf2b6f1cd2b50b675ba871
hkoma2jp <h.koma2.jp@gmail.com>
2023-02-04 18:14:29 +0900

    Lesson5 課題 プロ野球チームの勝ち負け数を管理しよう
    カプセル化

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -2,10 +2,10 @@ package baseball;
 
 public class BaseBallTeam {
     // フィールド
-    public String name;
-    public int win;
-    public int lose;
-    public int draw;
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
 
 
     // 引数なしコンストラクタ
@@ -39,6 +39,64 @@ public class BaseBallTeam {
             + "勝率は " + getRate(this.win, this.lose, this.draw)
             + "です。"
         );
+    }
+
+    // getter / setter
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name セットする name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return win
+     */
+    public int getWin() {
+        return win;
+    }
+
+    /**
+     * @param win セットする win
+     */
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    /**
+     * @return lose
+     */
+    public int getLose() {
+        return lose;
+    }
+
+    /**
+     * @param lose セットする lose
+     */
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+
+    /**
+     * @return draw
+     */
+    public int getDraw() {
+        return draw;
+    }
+
+    /**
+     * @param draw セットする draw
+     */
+    public void setDraw(int draw) {
+        this.draw = draw;
     }
 
 }
